2c76ec564af3de66a65a049355c4220b3801391c
stormymayday <stormymayday7@gmail.com>
2020-06-06 15:16:19 -0600

    Added an event listener to the 'new game' button and implemented the game initalization function.

--- app.js
+++ app.js
@@ -12,18 +12,8 @@ GAME RULES:
 // Game Variables
 var scores, roundScore, activePlayer;
 
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
-
-// Hiding the dice image
-document.querySelector('.dice').style.display = 'none';
-
-// Setting scores to zero
-document.getElementById('score-0').textContent = 0;
-document.getElementById('score-1').textContent = 0;
-document.getElementById('current-0').textContent = 0;
-document.getElementById('current-1').textContent = 0;
+// Initializing new game
+gameInit();
 
 // Roll Dice button Event Listener
 document.querySelector('.btn-roll').addEventListener('click', function () {
@@ -89,4 +79,34 @@ function nextPlayer() {
 
     // hiding the dice image
     document.querySelector('.dice').style.display = 'none';
+}
+
+// New Game button Event Listener
+document.querySelector('.btn-new').addEventListener('click', gameInit);
+
+function gameInit() {
+
+    // Removing the winner CSS class
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+
+    scores = [0, 0];
+    roundScore = 0;
+    activePlayer = 0;
+
+    // Adding back the active CSS class
+    document.querySelector('.player-' + activePlayer + '-panel').classList.add('active');
+
+    // Hiding the dice image
+    document.querySelector('.dice').style.display = 'none';
+
+    // Resetting the scores
+    document.getElementById('score-0').textContent = 0;
+    document.getElementById('score-1').textContent = 0;
+    document.getElementById('current-0').textContent = 0;
+    document.getElementById('current-1').textContent = 0;
+
+    // Reseting player names
+    document.getElementById('name-0').textContent = 'player 1';
+    document.getElementById('name-1').textContent = 'player 2';
 }
